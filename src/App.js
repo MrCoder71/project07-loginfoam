2feed6ebdead984ca6db870695221c455eab07a3
Mrcoder <amirsohail4151@gmail.com>
2024-06-28 23:55:13 -0700

    Login Foam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react'
+
+// import css file
 import './App.css';
 
-function App() {
+// import icon
+import { FaAt } from "react-icons/fa6";
+import { FaUnlockKeyhole } from "react-icons/fa6";
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    <form class="form">
+
+      <p id="heading">Login</p>
+
+
+      <div class="field">
+
+          <FaAt className='input-icon' />
+
+          <input autocomplete="off" placeholder="Username" class="input-field" type="text" />
+      
+      </div>
+
+
+      <div class="field">
+          
+          <FaUnlockKeyhole className='input-icon' />
+
+          <input placeholder="Password" class="input-field" type="password" />
+      
+      </div>
+
+      <div class="btn">
+          
+          <button class="button1">Login</button>
+          
+          <button class="button2">Sign Up</button>
+      
+      </div>
+      
+        <button class="button3">Forgot Password</button>
+    
+    </form>
+
+
+  )
 }
 
 export default App;
